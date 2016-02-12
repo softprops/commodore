19245ce83f5b9da2ba29694f880afe41e53fa6ac
softprops <d.tangren@gmail.com>
2016-02-12 08:43:25 -0500

    example use

---
+++ examples/server.rs
@@ -0,0 +1,22 @@
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
+extern crate commodore;
+extern crate hyper;
+
+use commodore::{Captures, Command, Mux, Response, Responder};
+use hyper::Server;
+
+pub fn main() {
+    env_logger::init().unwrap();
+    let addr = format!("0.0.0.0:{}", 4567);
+    let mut mux = Mux::new();
+    mux.command("/commadore", "secrettoken", |_: &Command, _: &Option<Captures>, _: Box<Responder>| -> Option<Response> {
+        None
+    });
+    let srvc = Server::http(&addr[..]).unwrap()
+        .handle(mux);
+    println!("listening on {}", addr);
+    srvc.unwrap();
+}
